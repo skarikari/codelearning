13731b2c08c84da02605549e02ac4f57508a30b5
Swift <skarikari8@gmail.com>
2022-09-22 11:59:44 -0700

    Added scope function

--- test_github.py
+++ test_github.py
@@ -1,1 +1,15 @@
 print('Hello this is my first commit')
+
+def func_0(start_num):
+    start_num += 1
+    return func_1(start_num)
+
+def func_1(start_num):
+    start_num += 1
+    return start_num
+
+start_num = 1
+print(f"start_num\t-> {start_num}")
+finish_num = func_0(start_num)
+print(f"calc'd_num\t-> {finish_num}")
+print(f"start_num\t-> {start_num}")
